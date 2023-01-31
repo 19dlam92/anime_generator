6cea16033c2ba688cf6c1db456977202a9cdaf20
19dlam92 <19dlam92@gmail.com>
2023-01-30 23:01:27 -0500

    initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+
+
+def get_anime():
+    anime_selection = input( "Please enter a genre. Generate random anime?" )
+    options = [
+        "adventure",
+        "action",
+        "romance",
+        "psycological thriller"
+
+        # include api of different genres?
+
+    ]
+
+    
