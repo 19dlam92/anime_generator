abfd688dbafc9541725c7445ad5fe1e70a4d832b
19dlam92 <19dlam92@gmail.com>
2023-03-02 15:07:41 -0500

    initial commit

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 import random
+from flask import Flask
+from jikanpy import Jikan
+
+'''
+episodes
+series
+movies
+genres
+'''
+
+
 
 
 def get_anime():
+    mood_selection = input("What're you in the mood for? (Series / Movies / Surprise Me!)")
+    # genre_selection 
     anime_selection = input( "Please enter a genre. Generate random anime?" )
     options = [
         "adventure",
@@ -12,5 +25,26 @@ def get_anime():
         # include api of different genres?
 
     ]
-
     
+    selection = ''
+    while True:
+        if mood_selection.lower == 'series':
+            print("You've selected series!")
+        else:
+            print("You've selected movies!")
+
+
+
+def get_manga():
+    book_selection = input("What're you interested in? (Light Novel / Comics / Surprise Me!)")
+    # genre_selection 
+    manga_selection = input( "Please enter a genre. Generate random manga?" )
+    options = [
+        
+
+        
+    ]
+
+    if book_selection.lower == 'light novel':
+        pass
+    elif 
